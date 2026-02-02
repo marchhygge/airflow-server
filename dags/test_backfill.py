138596e7dc36811dd349b493f36d7c4b504a11c2
0931844509 <hoangthinh110304@gmail.com>
2026-02-02 23:36:26 +0700

    update start_date

--- dags/test_backfill.py
+++ dags/test_backfill.py
@@ -5,7 +5,7 @@ from services.backfill_service import run_backfill
 
 with DAG(
     dag_id="fact_order_test_backfill_one_month_per_day",
-    start_date=datetime(2026, 2, 2, 9, 0),
+    start_date=datetime(2026, 2, 1, 9, 0),
     schedule="@daily",
     catchup=False,
     tags=["test", "backfill"],
