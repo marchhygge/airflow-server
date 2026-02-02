3c5197dac03f080d5689e979db8fbaf3dda8dc67
0931844509 <hoangthinh110304@gmail.com>
2026-02-02 21:06:57 +0700

    update dag

--- dags/test_backfill.py
+++ dags/test_backfill.py
@@ -1,13 +1,13 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
-from services.backfill_service import run_backfill
+from plugins.services.backfill_service import run_backfill
 
 with DAG(
     dag_id="fact_order_test_backfill_one_month_per_day",
     start_date=datetime(2026, 1, 22, 9, 0),
     schedule="@daily",
-    catchup=False,
+    catchup=True,
     tags=["test", "backfill"],
 ) as dag:
 
@@ -16,6 +16,7 @@ with DAG(
         python_callable=run_backfill,
         depends_on_past=True,
     )
+
 
 
 
