2b148cdf81ab797d3918f0bfa08855237a0a84c1
0931844509 <hoangthinh110304@gmail.com>
2026-02-14 23:20:28 +0700

    thinhh update backfill logic

--- plugins/services/backfill_services.py
+++ plugins/services/backfill_services.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from services.sql_services import get_max_date
 
-
+# Resolve raw SQL based on table existence
 def resolve_raw_sql(config, is_exist):
     """
     Decide CREATE or INSERT SQL block
@@ -15,7 +15,7 @@ def resolve_raw_sql(config, is_exist):
         return config['query']['function']['create'] + '\n' + config['query']['sql']
     return config['query']['function']['insert'] + '\n' + config['query']['sql']
 
-
+# Resolve start date for backfill
 def resolve_start_date_dt(pg_hook, schema, table, default_date, is_exist):
     """
     Decide backfill start month
@@ -32,7 +32,7 @@ def resolve_start_date_dt(pg_hook, schema, table, default_date, is_exist):
 
     return max_date + relativedelta(months=1)
 
-
+# Build SQL for a single backfill month
 def build_sql_for_month(raw_sql, schema, table, start_date_dt, render_template):
     """
     Render SQL for a single backfill month
