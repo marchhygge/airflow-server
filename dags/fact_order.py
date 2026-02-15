f8c9cf069a24a2d49d29e961cd437ea4d28bb29f
0931844509 <hoangthinh110304@gmail.com>
2026-02-15 21:13:15 +0700

    thinhh update backfill logic

--- dags/fact_order.py
+++ dags/fact_order.py
@@ -6,7 +6,7 @@ from services.backfill_orchestration import run_backfill
 
 with DAG(
     dag_id="fact_order_backfill",
-    start_date=datetime(2026, 2, 1, 9, 0),
+    start_date=datetime(2026, 13, 1, 9, 0),
     schedule="@daily",
     catchup=True,
     max_active_runs=1,
