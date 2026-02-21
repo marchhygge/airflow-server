1a5ca591b08a8857207e52f8f91e60f8ba0642bd
0931844509 <hoangthinh110304@gmail.com>
2026-02-21 21:54:11 +0700

    thinhh update method to_sql and add loginfor for api date and print sample dataframe

--- plugins/services/api_services.py
+++ plugins/services/api_services.py
@@ -74,7 +74,7 @@ def load_df_to_postgres(dataframe: pd.DataFrame, conn_id, schema, table_name):
         name=table_name,
         con=engine,
         schema=schema,
-        if_exists="append",
+        if_exists="replace", # Replace the table if it already exists
         index=False, # Do not write DataFrame index as a column in the database
         method="multi", # Use multi-row insert for better performance
         chunksize=1000 # Insert 1000 rows at a time
